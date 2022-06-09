79d3254a35253e2dc1890ec7266b8596cc055dec
Andrew-Shank <andrewshank03@gmail.com>
2022-06-09 15:29:51 -0400

    the last thing edwardo saw...

--- Code/Client/Main.py
+++ Code/Client/Main.py
@@ -18,7 +18,7 @@ from Client_Ui import Ui_Client
 from Video import *
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import *    
 from PyQt5.QtGui import *
 
 class mywindow(QMainWindow,Ui_Client):
