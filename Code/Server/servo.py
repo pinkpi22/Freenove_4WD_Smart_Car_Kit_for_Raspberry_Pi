4778e910ae272881e2e53589998aaabeaa65de58
m3ssm0r3m <colemanpeyton27@gmail.com>
2022-06-10 08:18:43 -0400

    updated pulse for channel 0 to be consistent
    Co-authored-by: atlas-77 <atlas-77@users.noreply.github.com>

--- Code/Server/servo.py
+++ Code/Server/servo.py
@@ -9,7 +9,7 @@ class Servo:
     def setServoPwm(self,channel,angle,error=10):
         angle=int(angle)
         if channel=='0':
-            self.PwmServo.setServoPulse(8,2500-int((angle+error)/0.09))
+            self.PwmServo.setServoPulse(8,500+int((angle+error)/0.09))
         elif channel=='1':
             self.PwmServo.setServoPulse(9,500+int((angle+error)/0.09))
         elif channel=='2':
