7ed4c10a9e7e5058cd4972b44e856114c9ce72fc
carloscuau10@icloud.com <carloscuau10@icloud.com>
2022-06-15 10:48:33 -0400

    Boundary Program- will get bot to go backwards when it detects a line

--- Code/Server/Boundary.py
+++ Code/Server/Boundary.py
@@ -20,15 +20,15 @@ class Boundary:
             if GPIO.input(self.IR03)==True:
                 self.LMR=(self.LMR | 1)
             if self.LMR==2:
-                PWM.setMotorModel(800,800,800,800)
+                PWM.setMotorModel(-800,-800,-800,-800)
             elif self.LMR==4:
-                PWM.setMotorModel(-1500,-1500,2500,2500)
+                PWM.setMotorModel(-1500,-1500,-2500,-2500)
             elif self.LMR==6:
-                PWM.setMotorModel(-2000,-2000,4000,4000)
+                PWM.setMotorModel(-2000,-2000,-4000,-4000)
             elif self.LMR==1:
-                PWM.setMotorModel(2500,2500,-1500,-1500)
+                PWM.setMotorModel(-2500,-2500,-1500,-1500)
             elif self.LMR==3:
-                PWM.setMotorModel(4000,4000,-2000,-2000)
+                PWM.setMotorModel(-4000,-4000,-2000,-2000)
             elif self.LMR==7:
                 #pass
                 PWM.setMotorModel(0,0,0,0)
