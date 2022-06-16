e339e34e7f54d61c0bd609f305643f5a108484f1
spumel534 <spumel534@gmail.com>
2022-06-16 15:01:29 -0400

    aghijhijhjihi

--- Code/Client/color_recognition.py
+++ Code/Client/color_recognition.py
@@ -12,17 +12,17 @@ class colors:
         rSum = 0
         gSum = 0
         bSum = 0
-        for i in range(10):
-            for j in range(10):
-                rSum += img[cy-5+i][cx-5+j][0]
-                gSum += img[cy-5+i][cx-5+j][1]
-                bSum += img[cy-5+i][cx-5+j][2]
+        for i in range(100):
+            for j in range(100):
+                rSum += img[cy-50+i][cx-5+j][0]
+                gSum += img[cy-50+i][cx-5+j][1]
+                bSum += img[cy-50+i][cx-5+j][2]
 
         b, g, r = int(rSum/100), int(gSum/100), int(bSum/100)
         colo = [r,g,b]
         print("r: " + str(r) + " | "+"g: " + str(g) + " | "+"b: " + str(b) + " | ")
-        if r < 10 and g > 90:
-            colo = [0,255,0]
+        if b < 150 and g < 150 and r < 10:
+            colo = 
         return colo
 
 
