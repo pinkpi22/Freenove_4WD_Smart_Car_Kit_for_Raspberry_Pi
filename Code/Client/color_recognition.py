344e5e16b7167ef37b3257b99e92f5b40708ac25
spumel534 <spumel534@gmail.com>
2022-06-16 14:13:39 -0400

    asdasdasd

--- Code/Client/color_recognition.py
+++ Code/Client/color_recognition.py
@@ -8,7 +8,7 @@ class colors:
     def run(img,xwid,ywid):
         cx = int(xwid / 2)
         cy = int(ywid / 2)
-        pixel_center = img[cy, cx]
+        pixel_center = img[cy - 10, cx]
         b, g, r = int(pixel_center[0]), int(pixel_center[1]), int(pixel_center[2])
         colo = [r,g,b]
         if r < 10 and g > 50 and b > 10:
